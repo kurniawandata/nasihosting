5e69d065fd6e928815e325a710d2345580eb531e
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2020-06-23 06:19:52 +0700

    Update nasihosting.sh

--- nasihosting.sh
+++ nasihosting.sh
@@ -9,7 +9,7 @@ clear
 echo "=================================================================";
 echo " Nasihosting for Apache Server (Ubuntu Server)                   ";
 echo " Progammer : Kurniawan. xcode.or.id                              ";
-echo " Version 1.0 Beta 23 - 22/06/2020                                ";
+echo " Version 1.0 Beta 24 - 22/06/2020                                ";
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=";
 echo " Instalasi                                                       ";
 echo " [1]  Install PHP 7.4 dan salin phpinfo.php ke /var/www/html     ";
