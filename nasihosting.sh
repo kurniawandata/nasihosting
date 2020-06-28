22a31b51cb33ebf4f0d9407ac3121e9259d99f46
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2020-06-28 07:11:49 +0700

    Update nasihosting.sh

--- nasihosting.sh
+++ nasihosting.sh
@@ -247,7 +247,7 @@ case $choice in
 *)    echo "Maaf, menu tidak ada"
 esac
 echo ""
-echo "Nasihosting for apache server (Ubuntu Server) - Beta 30"
+echo "Nasihosting for apache server (Ubuntu Server) - Beta 31"
 echo "Oleh Kurniawan - trainingxcode@gmail.com. xcode.or.id"
 echo ""
 echo -n "Kembali ke menu? [y/n]: ";
