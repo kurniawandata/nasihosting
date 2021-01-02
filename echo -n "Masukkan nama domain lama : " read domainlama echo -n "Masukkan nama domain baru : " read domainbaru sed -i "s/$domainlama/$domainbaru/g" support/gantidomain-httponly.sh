3f949a225c662217e6a9be743f6b3000c34cf825
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2021-01-02 15:24:21 -0800

    Delete gantidomain-httponly.sh

--- echo -n "Masukkan nama domain lama : " read domainlama echo -n "Masukkan nama domain baru : " read domainbaru sed -i "s/$domainlama/$domainbaru/g" support/gantidomain-httponly.sh
+++
@@ -1,6 +0,0 @@
-echo -n "Masukkan nama domain lama : "
-read domainlama
-echo -n "Masukkan nama domain baru : "
-read domainbaru
-sed -i "s/$domainlama/$domainbaru/g" support/subdomain2.conf
-service apache2 restart
