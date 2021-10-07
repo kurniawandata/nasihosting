b968707be9aa8644ef7e9794f39e1917645b8f03
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2021-10-07 01:07:04 -0700

    Update nasihosting-httponly.sh

--- nasihosting-httponly.sh
+++ nasihosting-httponly.sh
@@ -9,7 +9,7 @@ clear
 echo "=================================================================";
 echo " Nasihosting for Apache Server (Ubuntu Server) - http only       ";
 echo " Progammer : Kurniawan. xcode.or.id                              ";
-echo " Version 5.1 - 07/10/2020                                        ";
+echo " Version 5.2 - 07/10/2020                                        ";
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=";
 echo " Instalasi                                                       ";
 echo " [1]  Install PHP 8.0, salin phpinfo.php ke /var/www/html dan    ";
@@ -73,6 +73,9 @@ case $choice in
     sudo wget https://xcode.or.id/phpMyAdmin-5.1.1-all-languages.tar.gz
     sudo tar xzf phpMyAdmin-5.1.1-all-languages.tar.gz
     sudo mv phpMyAdmin-5.1.1-all-languages/* /usr/share/phpmyadmin
+    sudo mkdir /usr/share/phpmyadmin/tmp/
+    chmod 777 /usr/share/phpmyadmin/tmp/
+    sudo cp support/config.inc.php /usr/share/phpmyadmin/
     ;;
 
 3)  echo "Aktifkan /home.."
